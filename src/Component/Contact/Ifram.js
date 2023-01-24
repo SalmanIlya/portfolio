fa12cea05cba0a496359fdd6ff6b3a447912ff57
Salamanilyas <salamanilyas101@gmail.com>
2023-01-24 07:06:14 +0500

    contact

---
+++ src/Component/Contact/Ifram.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ifram = () => {
+  return (
+    <div className='text-center'>
+      <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Dilazak%20Road,%20Adil%20Medicos%20Peshawar+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure acres/hectares on map</a></iframe>
+    </div>
+  )
+}
+
+export default Ifram
