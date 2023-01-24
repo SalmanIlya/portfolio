35015e50a2248cf2ef5193b0e638fa1232329952
Salamanilyas <salamanilyas101@gmail.com>
2023-01-24 07:02:58 +0500

    contact-page-in-home-page

---
+++ src/Component/Home/Contact.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='contact-div '>
+<div className='container'>
+<h1 className='mt-5 pt-5'>Don’t Hesitate To Contact</h1>
+<div>
+    <p className='pt-5 h6'>Have a question? Get in touch now!</p>
+</div>
+<div>
+    <div className='d-flex flex-column pt-5 '>
+        <h4><strong>Write Me:</strong></h4>
+        <p>mfqureshi077@gmail.com</p>
+    </div>
+    <div className='d-flex flex-column pt-3'>
+        <h4><strong>Call Me:</strong></h4>
+        <p>+923424717112 / +923429813779 </p>
+    </div>
+    <div className='d-flex flex-column pt-3'>
+        <h4><strong>Visit My Office:</strong></h4>
+        <p>Plot No. 166, Road B-7, Hayatabad, Phase 1 Industrial Estate, Peshawar, Khyber Pakhtunkhwa
+</p>
+    </div>
+</div>
+
+</div>
+
+
+    </div>
+  )
+}
+
+export default Contact
