2e5fff991fa2e57ca3416d86c0006660dc8efb17
Salamanilyas <salamanilyas101@gmail.com>
2023-01-24 07:00:30 +0500

    routes-in-App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import About from './Component/About/About'
+import Contact from './Component/Contact/Contact'
+import Footer from './Component/Footer/Footer'
+import Home from './Component/Home/Home'
+import Navbar from './Component/Navbar/Navbar'
+import PageNotFound from './PageNotFound'
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <main>
+      <Routes>
+        <Route exect path='/' element={<Home/>} />
+        <Route exect path='/about' element={<About/>} />
+        <Route exect path='/contact' element={<Contact/>} />
+        <Route exect path='/*' element={<PageNotFound/>} />
+
+      </Routes>
+      </main>
+      <Footer/>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
